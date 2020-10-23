ed70e0f36b0a1a959a401a2fdecbdbfc37b9fe0b
DaddyMcQueen <72531288+DaddyMcQueen@users.noreply.github.com>
2020-10-23 18:25:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import numpy as np
+import pickle
 from deepnn.constants import SIZE_IN
 from deepnn.constants import SIZE_OUT
 from deepnn.constants import SIZE_HIDDEN
 from deepnn.constants import LEARNING_RATE
 from deepnn.constants import NUM_BATCHES
+from deepnn.constants import EPOCHS
 from nn import DeepNN
 from mnistload import Batch
 
@@ -11,43 +13,40 @@ from mnistload import Batch
 # MNIST Dataset problem
 # Deep Neural Network
 #
-# Version: 10-20-2020
+# Version: 10-23-2020
 #-----------------------#
-
-def create_network():
-	mnist_network = DeepNN(SIZE_IN, SIZE_OUT, SIZE_HIDDEN, LEARNING_RATE)
-
 def main():
-	create_network = True
-
-	if create_network == True:
-		mnist_network = DeepNN(SIZE_IN, SIZE_OUT, SIZE_HIDDEN, LEARNING_RATE)
-
-
-	train_network = True
-	
-	if train_network == True:
-		for i in range(598):
-			batch = Batch(i)
-			inputs = batch.batch_i
-			targets = batch.batch_l
-			mnist_network.train(inputs, targets)
-			if i >= 599:
-				pass
-				# print(mnist_network.w_in_hidden1)
-				# print(mnist_network.w_hidden1_hidden2)
-				# print(mnist_network.w_hidden2_out)
-				# print(mnist_network.bias_h1)
-				# print(mnist_network.bias_h2)
-				# print(mnist_network.bias_out)
-
+	new_network = True
 	test_network = True
 
+	if new_network:
+		mnist_network = DeepNN(SIZE_IN, SIZE_OUT, SIZE_HIDDEN, LEARNING_RATE)
+		train_network = True
+	else:
+		pickel_in = open("nn.pickel", "rb")
+		mnist_network = pickle.load(pickel_in)
+		train_network = False
+		
+	if train_network == True:
+
+		for i in range(EPOCHS):
+			for i in range(549):
+				batch = Batch(i)
+				inputs = batch.images
+				targets = batch.labels
+				mnist_network.train(inputs, targets)
+		pickle_out = open("nn.pickel", "wb")
+		pickle.dump(mnist_network, pickle_out)
+		pickle_out.close()
+				
+	# get testing data 
 	if test_network == True:
-		test_batch = Batch(599)
-		test_images = test_batch.batch_i
-		test_labels = test_batch.batch_l
-		print(test_labels[17])
-		mnist_network.forward_pass(test_images[17])
-		print(mnist_network.layer_out)
+		accuracy = 0
+		for i in range(50):
+			test_batch = Batch(i + 549)
+			test_images = test_batch.images
+			test_labels = test_batch.labels
+			accuracy += mnist_network.test_network(test_images, test_labels)
+		avg_accuracy = accuracy/50
+		print("The accuracy of the network on never before seen data is: ", avg_accuracy * 100, "%")
 main()
